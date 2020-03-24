c3586076d05a36fe526ff05250e5fdf87770fcf1
森本 <mrmt0814@gmail.com>
2020-03-24 15:54:28 +0900

    Add: Mainにシーン管理を追加

--- Oni/Oni/Src/Main.cpp
+++ Oni/Oni/Src/Main.cpp
@@ -1,10 +1,30 @@
-#include<Siv3D.hpp>
+#include"MyLibrary.hpp"
+
+// シーン
+#include"Title/TitleScene.hpp"
 
 
 void Main()
 {
+	// ウィンドウの設定
+	Window::Resize(640, 480);
+	Window::SetTitle(U"鬼");
+
+	// アセット管理
+	FontAsset::Register(U"10", 10, Typeface::Medium);
+	FontAsset::Register(U"15", 15, Typeface::Medium);
+	FontAsset::Register(U"20", 20, Typeface::Medium);
+	FontAsset::Register(U"30", 30, Typeface::Medium);
+	FontAsset::Register(U"40", 40, Typeface::Medium);
+
+	// シーンの管理
+	MyApp sceneManager;
+	sceneManager.add<Oni::TitleScene>(SceneName::TITLE);
+
 	while (System::Update())
 	{
+		sceneManager.update();
 
+		Oni::showFps();
 	}
 }
