bf8e6c87e37de30880195edc00e3bd1708f59139
森本 <mrmt0814@gmail.com>
2020-04-24 22:57:57 +0900

    Add: TextBoardクラスの作成

---
+++ Oni/Oni/Src/Game/Board/Text/TextBoard.cpp
@@ -0,0 +1,66 @@
+#include "TextBoard.hpp"
+
+
+namespace
+{
+	// 一文字を描画するのにかける時間
+	constexpr double ONE_CHAR_DRAW_SPAN = 0.1;
+
+	// 範囲
+	constexpr Rect REGION(0, 0, 640, 120);
+
+	// アイコンを描画する座標
+	constexpr Point ICON_DRAW_POS(16, 16);
+
+	// 話し手の名前を描画する座標
+	constexpr Point SPEAKER_DRAW_POS(60, 25);
+
+	// テキストを表示する座標
+	constexpr Point DRAW_TEXT_POS(136, 16);
+}
+
+
+namespace Oni
+{
+
+	TextBoard::TextBoard(const String& iconName, const String& speakerName, const String& text)
+		: GameBoard(REGION)
+		, mDrawAbleLength(text.size()* ONE_CHAR_DRAW_SPAN, text.size())
+		, mIconName(iconName)
+		, mSpeakerName(speakerName)
+		, mText(text)
+	{
+	}
+
+
+	void TextBoard::select()
+	{
+
+	}
+
+
+	void TextBoard::update()
+	{
+
+	}
+
+
+	void TextBoard::selectedUpdate()
+	{
+		mDrawAbleLength.update();
+	}
+
+
+	void TextBoard::draw() const
+	{
+		if (mIconName != U"")
+		{
+			TextureAsset(mIconName).draw(ICON_DRAW_POS);
+		}
+
+		FontAsset(U"20")(mSpeakerName).drawAt(SPEAKER_DRAW_POS);
+
+		FontAsset(U"20")(mText.substr(0, mDrawAbleLength.getValue())).draw(DRAW_TEXT_POS);
+	}
+
+}
---
+++ Oni/Oni/Src/Game/Board/Text/TextBoard.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+
+#include "../GameBoard.hpp"
+#include "../../../Slice/Linearly.hpp"
+
+
+namespace Oni
+{
+	/// <summary>
+	/// テキストを表示するウィンドウ
+	/// </summary>
+	class TextBoard : public GameBoard
+	{
+	private:
+
+		// 表示文字数
+		Linearly<size_t> mDrawAbleLength;
+
+		// アイコンの画像名
+		const String mIconName;
+
+		// 話し手の名前
+		const String mSpeakerName;
+
+		// テキスト
+		const String mText;
+
+	public:
+
+		/// <summary>
+		/// テキストウィンドウ
+		/// </summary>
+		/// <param name="iconName"   > アイコンの画像名 </param>
+		/// <param name="speakerName"> 話し手の名前     </param>
+		/// <param name="text"       > テキスト         </param>
+		TextBoard(const String& iconName, const String& speakerName, const String& text);
+
+		void select() override;
+
+		void update() override;
+
+		void selectedUpdate() override;
+
+		void draw() const override;
+
+	};
+}
